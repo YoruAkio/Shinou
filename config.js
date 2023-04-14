83645897face0d655ce05d678bfe3bcc55e79b52
YoruAkio <yoruakio1124@gmail.com>
2023-04-15 06:52:14 +0700

    🎀 Add config file

---
+++ config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    BOT: {
+        TEST_SERVER: "962411933829562430",
+        CLIENT_ID: "920954192523329546",
+        DEVS: ["919841186246692886"],
+    },
+    EMBED_COLORS: {
+        BOT_EMBED: "#068ADD",
+        TRANSPARENT: "#36393F",
+        SUCCESS: "#00A56A",
+        ERROR: "#D61A3C",
+        WARNING: "#F7E919",
+    },
+    MODERATION: {
+        ENABLED: false,
+        EMBED_COLORS: {
+            TIMEOUT: "#102027",
+            UNTIMEOUT: "#4B636E",
+            KICK: "#FF7961",
+            SOFTBAN: "#AF4448",
+            BAN: "#D32F2F",
+            UNBAN: "#00C853",
+            VMUTE: "#102027",
+            VUNMUTE: "#4B636E",
+            DEAFEN: "#102027",
+            UNDEAFEN: "#4B636E",
+            DISCONNECT: "RANDOM",
+            MOVE: "RANDOM",
+        },
+    },
+    WEBHOOKS: {
+        ERROR_LOGS:
+            "https://discord.com/api/webhooks/983321129194242048/h2B8BisgvzMO5V0Bxi6WMlQiZmWTik-G27gcRelatxyA2faJbcmySUClZgLw7kI52M7N",
+    },
+};
