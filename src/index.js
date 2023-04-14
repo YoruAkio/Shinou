83e7cfde155ec8a9066559ce954d8c0f6abd7985
YoruAkio <yoruakio1124@gmail.com>
2023-04-15 04:43:35 +0700

    💾 Connect To Database

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { Client, IntentsBitField } = require("discord.js");
+const mongoose = require("mongoose");
 const eventHandler = require("./handlers/eventHandler");
 
 const client = new Client({
@@ -12,6 +13,18 @@ const client = new Client({
 });
 
 eventHandler(client);
+
+(async () => {
+    try {
+        mongoose.set("strictQuery", false);
+        await mongoose.connect(process.env.MONGODB_URI, { keepAlive: true });
+        console.log("🧶 Connected to DB.");
+
+        eventHandler(client);
+    } catch (error) {
+        console.log(`Error: ${error}`);
+    }
+})();
 
 client.config = require("../config.json");
 client.login(process.env.TOKEN);
