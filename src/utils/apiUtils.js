d1b74d62b3a33142c531b5e46c1ca61307adfe02
YoruAkio <yoruakio1124@gmail.com>
2023-04-15 23:18:59 +0700

    ⚙ Add more image command and new APIs utils

---
+++ src/utils/apiUtils.js
@@ -0,0 +1,25 @@
+const nekos = require("nekos.life");
+const neko = new nekos();
+
+async function getNeko(category) {
+    return (await neko[category]()).url;
+}
+
+async function getWaifu(category) {
+    let imageUrl;
+
+    if (!category) throw new Error("No category provided");
+
+    const response = await httpUtils.getJson(
+        `https://api.waifu.pics/sfw/${category}`
+    );
+    if (!response.success) throw new Error("API error");
+    imageUrl = response.data.url;
+
+    return imageUrl;
+}
+
+module.exports = {
+    getNeko,
+    getWaifu,
+};
