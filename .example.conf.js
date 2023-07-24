18183f283b838461e3ff9535e0c050428eda617b
YoruAkio <97880708+YoruAkio@users.noreply.github.com>
2023-07-24 17:11:34 +0000

    * ✨ feat: .example.conf.jsと.example.envファイルを追加 * * .example.conf.jsファイルは、Botの設定やカラーコード、Webhooksの設定を含んでいます。 * * .example.envファイルは、TOKENとMONGODB_URIの環境変数を設定するためのファイルです。

---
+++ .example.conf.js
@@ -0,0 +1,18 @@
+module.exports = {
+    Bot: {
+        test_server: 'YOUR_TEST_SERVER_ID',
+        client_id: 'YOUR_BOT_ID',
+        devs: ['YOUR_DISCORD_ID', 'YOUR_DISCORD_SECOND_ID'],
+    },
+    Colors: {
+        BOT_EMBED: '#068ADD',
+        TRANSPARENT: '#36393F',
+        SUCCESS: '#00A56A',
+        ERROR: '#D61A3C',
+        WARNING: '#F7E919',
+        PINK: '#FF91A4',
+    },
+    Webhooks: {
+        ERROR_LOGS: '',
+    },
+};
